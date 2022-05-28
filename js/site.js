aaf0278447c9be506e52620099e71375985317ee
leomazzaferro <leonel.mazzaferro@gmail.com>
2022-05-27 23:20:55 -0300

    primer commit

---
+++ js/site.js
@@ -0,0 +1,161 @@
+const nave = document.getElementById("nave");
+
+const body = document.getElementById("body");
+
+const misil = document.getElementById("misil");
+
+const vida = document.getElementById("vida");
+
+const time = document.getElementById("times");
+
+let vidas = 5;
+
+let segundos = 60;
+
+
+
+setInterval(() => {
+
+    segundos--;
+
+    time.textContent = segundos;
+
+    if (segundos == 0) {
+
+        alert('You Win!');
+
+        location.reload();
+
+    }
+
+}, 1000);
+
+
+
+// POSICION NAVE
+
+document.addEventListener('mousemove', (e) => {
+
+    nave.style.left = (e.clientX - 40) + 'px';
+
+});
+
+
+
+// GENERAR DISPAROS
+
+document.addEventListener('click', () => {
+
+    let bala = document.createElement('div');
+
+    bala.classList.add('bala');
+
+    bala.style.bottom = 70 + 'px';
+
+    bala.style.left = (nave.getBoundingClientRect().left + 20) + 'px';
+    
+    body.append(bala);
+    
+});
+
+
+
+// MOVIMIENTO DE DISPAROS
+
+setInterval(() => {
+
+    let balas = document.querySelectorAll('.bala');
+
+    balas.forEach(bala => {
+
+        bala.style.top = (bala.getBoundingClientRect().top - 20) + 'px';
+
+        if (bala.getBoundingClientRect().top <= 0) {
+
+            bala.remove();
+
+        }
+
+        // DISPARO ACERTADO
+
+        let enemigos = document.querySelectorAll('.enemigo');
+        
+        enemigos.forEach(enemigo => {
+
+            if (bala.getBoundingClientRect().top <= enemigo.getBoundingClientRect().top + 20) {
+
+                if (bala.getBoundingClientRect().left >= enemigo.getBoundingClientRect().left && bala.getBoundingClientRect().left <= enemigo.getBoundingClientRect().left + 20) {
+                    
+                    console.log("hit!")
+                    
+                    enemigo.style.backgroundImage = 'url("img/explosion.png")';
+                    
+                    setTimeout(() => {
+
+                        enemigo.remove();
+
+                        bala.remove();
+
+                    }, 100);
+
+                }
+
+            }
+
+        });
+
+    })
+
+}, 100);
+
+// GENERAR ENEMIGOS
+
+let aparecer = 0;
+
+setInterval(() => {
+
+    aparecer++;
+
+    if (aparecer % 10 == 0) {
+
+        let enemigo = document.createElement('div');
+
+        enemigo.classList.add('enemigo');
+
+        body.append(enemigo);
+
+        enemigo.style.left = (Math.random() * window.innerWidth - 50) + 'px';
+
+    }
+
+
+
+    // MOVIMIENTO DE ENEMIGOS
+
+    let enemigos = document.querySelectorAll('.enemigo');
+
+    enemigos.forEach(element => {
+
+        element.style.top = (element.getBoundingClientRect().top + 10) + 'px';
+
+        if (element.getBoundingClientRect().top > nave.getBoundingClientRect().top) {
+
+            vidas--;
+
+            vida.textContent = vidas;
+
+            if (vidas == -1) {
+
+                alert("GAME OVER");
+
+                location.reload();
+
+            }
+
+            element.remove();
+
+        }
+
+    });
+    
+}, 150)
